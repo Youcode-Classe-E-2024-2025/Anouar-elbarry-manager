4c398b40e98045e2a93820bfbc9ee97d4f7de957
anouar el barry <elbarryanwar37@gmail.com>
2024-12-17 15:00:27 +0100

    creat table for products

--- dashboards/admin_dashboard.php
+++ dashboards/admin_dashboard.php
@@ -36,7 +36,7 @@
     </div>
   </div>
 </nav>
-
+<!-- aside -->
 <aside id="logo-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700" aria-label="Sidebar">
    <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
       <ul class="space-y-2 font-medium">
@@ -95,37 +95,70 @@
       </ul>
    </div>
 </aside>
-   
- <h2 class="text-5xl font-bold ml-4 mb-5 dark:text-white">Authors Table</h2>
 
-        <table class="w-full text-sm text-left rtl:text-center text-gray-500 dark:text-gray-400">
-            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                <tr>
-                    <th scope="col" class="px-6 py-3">
-                        ID
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        Email
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        Name
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        Bio
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        <span class="sr-only">Edit</span>
-                    </th>
-                    <th scope="col" class="px-6 py-3">
-                        <span class="sr-only">Delet</span>
-                    </th>
-                </tr>
-            </thead>
-            <tbody>
-               
-            </tbody>
-        </table>
-    </div>
+<!-- products table -->
+<div class="mt-14 authors_table h-screen sm:ml-64 overflow-x-auto shadow-md sm:rounded-lg">
+   
+   <h2 class="text-5xl font-bold ml-4 mb-5 dark:text-white">Authors Table</h2>
+  
+          <table class="w-full text-sm text-left rtl:text-center text-gray-500 dark:text-gray-400">
+              <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                  <tr>
+                      <th scope="col" class="px-6 py-3">
+                          ID
+                      </th>
+                      <th scope="col" class="px-6 py-3">
+                      Name
+                      </th>
+                      <th scope="col" class="px-6 py-3">
+                      Category
+                      </th>
+                      <th scope="col" class="px-6 py-3">
+                      Supplier
+                      </th>
+                      <th scope="col" class="px-6 py-3">
+                      Quantity in Stock
+                      </th>
+                      <th scope="col" class="px-6 py-3">
+                      Reorder Level
+                      </th>
+                      <th scope="col" class="px-6 py-3">
+                       Actions
+                      </th>
+                  </tr>
+              </thead>
+              <tbody>
+              <tr
+                      class='bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600'>
+                      <td class='px-6 py-4'>
+                        YOK
+                      </td>
+                      <td class='px-6 py-4'>
+                        YOK
+                      </td>
+                      <td class='px-6 py-4'>
+                          234
+                      </td>
+                      <td class='px-6 py-4'>
+                     23
+                      </td>
+                      <td class='px-6 py-4'>
+                     23
+                      </td>
+                      <td class='px-6 py-4'>
+                     23
+                      </td>
+                                   
+                      <td class='px-2 py-4 flex  justify-around'>
+                          <a href='#' class='font-medium text-blue-600 dark:text-blue-500 hover:underline'>Edit</a>
+                          <a href="#" class='font-medium text-red-600 dark:text-red-500 hover:underline'>delet</a>
+                      </td>
+                  </tr>
+              </tbody>
+          </table>
+      </div>
+
+
     <script>
 
       const menu = document.getElementById('menu');
