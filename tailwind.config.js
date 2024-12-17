d2cff621be81b28724fa8e90790ef8e886f40892
anouar el barry <elbarryanwar37@gmail.com>
2024-12-17 13:02:03 +0100

    setup tailwind environmen

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}","index.php","dashboards/admin_dashboard.php",
+    "dashboards/user_dashboard.php"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
