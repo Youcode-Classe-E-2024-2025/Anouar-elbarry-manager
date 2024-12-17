7f5516ce97956e51305b3cfaf8c4e330cc886127
anouar el barry <elbarryanwar37@gmail.com>
2024-12-17 20:55:42 +0100

    creat users table

--- database/database.sql
+++ database/database.sql
@@ -3,3 +3,12 @@ CREATE TABLE role
 id INT AUTO_INCREMENT PRIMARY KEY,
 role_name VARCHAR(50) NOT NULL, 
 );
+
+CREATE TABLE app_user (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    user_password VARCHAR(50) NOT NULL UNIQUE,
+    role_id INT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (role_id) REFERENCES role(id)
+);
