69cd1b481433565ebb710d6eda04da90bddf4c19
anouar el barry <elbarryanwar37@gmail.com>
2024-12-18 09:13:14 +0100

    add email to user table

--- database/database.sql
+++ database/database.sql
@@ -7,6 +7,7 @@ role_name VARCHAR(50) NOT NULL,
 CREATE TABLE app_user (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
     user_password VARCHAR(50) NOT NULL UNIQUE,
     role_id INT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
