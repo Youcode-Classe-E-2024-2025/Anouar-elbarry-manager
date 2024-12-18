d0dbc94eda9839044aaae4c8b22939de856af71e
anouar el barry <elbarryanwar37@gmail.com>
2024-12-18 16:33:16 +0100

    add all modals into the configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}","index.php","dashboards/admin_dashboard.php",
-    "dashboards/user_dashboard.php"],
+    "dashboards/user_dashboard.php","src/modal/supplier/add_supplier.php","src/modal/categories/add_category.php","src/modal/users/add_user.php"],
   theme: {
     extend: {},
   },
