0c3074307f1eca700da85b6a737788f441242144
anouar el barry <elbarryanwar37@gmail.com>
2024-12-18 16:37:42 +0100

    creat buttons for (add (product/category/user))

--- dashboards/admin_dashboard.php
+++ dashboards/admin_dashboard.php
@@ -1,6 +1,8 @@
 <?php
 require_once('./../database/configuration.php');
 require_once('./../src/modal/supplier/add_supplier.php');
+require_once('./../src\modal\categories\add_category.php');
+require_once('./../src\modal\products\add_product.php');
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -12,7 +14,7 @@ require_once('./../src/modal/supplier/add_supplier.php');
     <title>STOCK WISE Admin</title>
 </head>
 
-<body style="background-image: url('./../src/imges/bg.png'); background-size:cover;">
+<body>
 
     <!-- navbare -->
     <nav class="fixed top-0 z-50 w-full bg-black border-b border-gray-600">
@@ -171,7 +173,7 @@ require_once('./../src/modal/supplier/add_supplier.php');
                 <!-- add supplier -->
                 <li>
                     <a href="#"
-                     id="suppliers"
+                     id="add_supplier"
                         class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                         <svg xmlns="http://www.w3.org/2000/svg"
                         class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-orange-600 dark:group-hover:text-white"
@@ -183,12 +185,61 @@ require_once('./../src/modal/supplier/add_supplier.php');
                         <span class="flex-1 ms-3 whitespace-nowrap">Add new Supplier</span>
                     </a>
                 </li>
+
+                <!-- add Category -->
+                <li>
+                    <a href="#"
+                     id="add_category"
+                        class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                        <svg xmlns="http://www.w3.org/2000/svg"
+                        class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-orange-600 dark:group-hover:text-white"
+                         fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+</svg>
+
+
+                        <span class="flex-1 ms-3 whitespace-nowrap">Add new Category</span>
+                    </a>
+                </li>
+                
+                <!-- add Product -->
+                <li>
+                    <a href="#"
+                     id="add_product"
+                        class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                        <svg xmlns="http://www.w3.org/2000/svg"
+                        class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-orange-600 dark:group-hover:text-white"
+                         fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+</svg>
+
+
+                        <span class="flex-1 ms-3 whitespace-nowrap">Add new Product</span>
+                    </a>
+                </li>
+
+                <!-- add user -->
+                <li>
+                    <a href="#"
+                     id="add_product"
+                        class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                        <svg xmlns="http://www.w3.org/2000/svg"
+                         class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-orange-600 dark:group-hover:text-white"
+                        fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z" />
+</svg>
+
+
+
+                        <span class="flex-1 ms-3 whitespace-nowrap">Add new Product</span>
+                    </a>
+                </li>
             </ul>
         </div>
     </aside>
 
     <!-- home -->
-    <section id="home_section" class="h-screen sm:ml-64 overflow-x-auto shadow-md sm:rounded-lg relative">
+    <section style="background-image: url('./../src/imges/bg.png'); background-size:cover;" id="home_section" class="h-screen sm:ml-64 overflow-x-auto shadow-md sm:rounded-lg relative">
   <div class="absolute inset-0 bg-black opacity-50"></div>
   <div class="relative z-10 text-center text-white pt-40">
     <h1 class="text-4xl md:text-5xl font-bold leading-tight">Welcome to <span class="text-emerald-500">STOK</span> <span class="text-orange-600">WISE</span></h1>
