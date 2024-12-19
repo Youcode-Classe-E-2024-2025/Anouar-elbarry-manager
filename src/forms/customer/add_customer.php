fd5a5ec477c8166a1a8572ee1cb2dc088b8a3204
anouar el barry <elbarryanwar37@gmail.com>
2024-12-19 17:26:41 +0100

    change paths and inputs

--- src/forms/customer/add_customer.php
+++ src/forms/customer/add_customer.php
@@ -1,19 +1,19 @@
 <div class="hidden pt-14 addcustomer_modal h-screen sm:ml-64 overflow-x-auto shadow-md sm:rounded-lg px-8">
-        <h2 class="text-2xl font-bold mb-4">Supplier Information</h2>
-        <form action="./src/controllers/authorController.php" class="grid grid-cols-2 gap-6" method="POST">
+        <h2 class="text-2xl font-bold mb-4">Customer Information</h2>
+        <form action="./../src\Controllers\customer_Controller.php" class="grid grid-cols-2 gap-6" method="POST">
             <div class="mb-4 col-start-1">
-                <label for="name" class="block text-gray-700 font-medium mb-2">First Name</label>
-                <input type="text" id="name" name="name" class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required>
+                <label for="firstName" class="block text-gray-700 font-medium mb-2">First Name</label>
+                <input type="text" id="firstName" name="firstName" class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required>
             </div>
 
             <div class="mb-4 col-start-2">
-                <label for="email" class="block text-gray-700 font-medium mb-2">Last Name</label>
-                <input type="email" id="email" name="email" class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required>
+                <label for="LastName" class="block text-gray-700 font-medium mb-2">Last Name</label>
+                <input type="text" id="LastName" name="LastName" class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required>
             </div>
 
             <div class="mb-4 col-start-1">
-                <label for="number" class="block text-gray-700 font-medium mb-2">Email</label>
-                <input type="tel" id="number" name="phone" class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required>
+                <label for="email" class="block text-gray-700 font-medium mb-2">Email</label>
+                <input type="email" id="email" name="email" class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required>
             </div>
             <div class="mb-4 col-start-2">
                 <label for="number" class="block text-gray-700 font-medium mb-2">Phone</label>
@@ -21,9 +21,9 @@
             </div>
             <div class="mb-4 col-start-1">
                 <label for="address" class="block text-gray-700 font-medium mb-2">Adress</label>
-                <input type="text" id="address" name="adress" class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required>
+                <input type="text" id="address" name="address" class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required>
             </div>
-            <div class="col-start-2 flex justify-end">
+            <div class="col-start-2 flex items-center justify-end pr-7 ">
                 <button type="submit" class="bg-yellow-500 text-white px-4 py-2 rounded-lg hover:bg-yellow-600">Submit</button>
             </div>
             <div class="hidden justify-end">
