2ab7809e256a2006834b18f6640c9b937a3f1b98
anouar el barry <elbarryanwar37@gmail.com>
2024-12-19 12:09:54 +0100

    add all controller files into the tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}","index.php","dashboards/admin_dashboard.php",
-    "dashboards/user_dashboard.php","src/modal/supplier/add_supplier.php","src/modal/categories/add_category.php","src/modal/users/add_user.php"],
+    "dashboards/user_dashboard.php","src/modal/supplier/add_supplier.php","src/modal/categories/add_category.php","src/modal/users/add_user.php",
+    "src/modal/order/add_order.php",
+     "src/Controllers/product_Controler.php",
+     "src/Controllers/category_Controler.php",
+     "src/Controllers/order_Controler.php",
+     "src/Controllers/supplier_Controler.php",
+     "src/Controllers/customer_Controler.php",
+     "src/Controllers/user_Controler.php"
+    ],
   theme: {
     extend: {},
   },
