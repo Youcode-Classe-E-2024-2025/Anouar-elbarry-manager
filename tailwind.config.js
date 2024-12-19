69a7cffc9dd09ea5bf00630b05b7ba2aeb6c4fb1
anouar el barry <elbarryanwar37@gmail.com>
2024-12-19 15:37:27 +0100

    change folders name from model to forms

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}","index.php","dashboards/admin_dashboard.php",
-    "dashboards/user_dashboard.php","src/modal/supplier/add_supplier.php","src/modal/categories/add_category.php","src/modal/users/add_user.php",
-    "src/modal/order/add_order.php",
+    "dashboards/user_dashboard.php",
+    "src/forms/supplier/add_supplier.php",
+    "src/forms/products/add_product.php",
+    "src/forms/categories/add_category.php",
+    "src/forms/users/add_user.php",
+    "src/forms/order/add_order.php",
      "src/Controllers/product_Controler.php",
      "src/Controllers/category_Controler.php",
      "src/Controllers/order_Controler.php",
