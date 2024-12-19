48f58e02d7ebca774f6c7269467150d395255ead
anouar el barry <elbarryanwar37@gmail.com>
2024-12-19 16:45:50 +0100

    creat form for add new category

---
+++ src/forms/categories/add_category.php
@@ -0,0 +1,19 @@
+<div class="hidden pt-14 addcategory_modal h-screen sm:ml-64 overflow-x-auto shadow-md sm:rounded-lg px-32">
+        <h2 class="text-2xl font-bold mb-4">Category Information</h2>
+        <form action="./src/controllers/authorController.php" method="POST">
+            <div class="mb-4">
+                <label for="name" class="block text-gray-700 font-medium mb-2">Name</label>
+                <input type="text" id="name" name="name" class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required>
+            </div>
+            <div class="mb-4">
+                <label for="email" class="block text-gray-700 font-medium mb-2">products Nbr</label>
+                <input type="email" id="email" name="email" class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required>
+            </div>
+            <div class="flex justify-end">
+                <button type="submit" class="bg-yellow-500 text-white px-4 py-2 rounded-lg hover:bg-yellow-600">Submit</button>
+            </div>
+            <div class="hidden  justify-end">
+                <button type="submit" class="bg-green-500 text-white px-4 py-2 rounded-lg hover:bg-green-600">update</button>
+            </div>
+        </form>
+    </div>    
