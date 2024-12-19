61c3fa6814e3fe656f8277bac3c4d30b8b0f8472
anouar el barry <elbarryanwar37@gmail.com>
2024-12-20 00:19:21 +0100

    creat input to select products

--- src/forms/order/add_order.php
+++ src/forms/order/add_order.php
@@ -4,13 +4,29 @@ require_once './../database/configuration.php';
 <div class="hidden pt-14 Addorder_modal h-screen sm:ml-64 overflow-x-auto shadow-md sm:rounded-lg px-8">
         <h2 class="text-2xl font-bold mb-4">Product Information</h2>
         <form action="./../src/controllers/packageController.php" method="POST">
+           
             <div class="mb-4">
-                <label for="name" class="block text-gray-700 font-medium mb-2">Name</label>
-                <input type="text" id="name" name="name" class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required>
+                <label for="product" class="block text-gray-700 font-medium mb-2">Product</label>
+                <select name="product" id="product"  class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required>
+                <option value="choose product" selected>choose product</option>
+                    <?php
+                $sql = "SELECT `name` FROM product";
+                $result = $conn->query($sql);
+                if (!$result) {
+                    die("Invalid query:" . $conn->error);
+                }
+                //read data of each row
+                while ($option = $result->fetch_assoc()) {
+                    echo "
+                   <option id='$option[name]'>$option[name]</option>
+                    ";
+                }
+                ?>
+                </select>
             </div>
             <div class="mb-4">
-                <label for="supplier" class="block text-gray-700 font-medium mb-2">customer</label>
-                <select name="supplier" id="supplier"  class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required>
+                <label for="customer_email" class="block text-gray-700 font-medium mb-2">customer</label>
+                <select name="customer" id="customer_email"  class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required>
                 <option value="choose customer's email" selected>choose customer's email</option>
                     <?php
                 $sql = "SELECT email FROM customer";
