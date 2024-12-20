15dbf84a0ffe3ef204f371e9cec672d12cf8d82b
anouar el barry <elbarryanwar37@gmail.com>
2024-12-20 20:51:15 +0100

    change action path

--- src/forms/products/add_product.php
+++ src/forms/products/add_product.php
@@ -3,7 +3,7 @@ require_once './../database/configuration.php';
 ?>
 <div class="hidden pt-14 Addproduct_modal h-screen sm:ml-64 overflow-x-auto shadow-md sm:rounded-lg px-8">
         <h2 class="text-2xl font-bold mb-4">Product Information</h2>
-        <form action="./../src/controllers/packageController.php" method="POST">
+        <form action="./../src\Controllers\product_Controler.php" method="POST">
             <div class="mb-4">
                 <label for="name" class="block text-gray-700 font-medium mb-2">Name</label>
                 <input type="text" id="name" name="name" class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required>
@@ -18,8 +18,8 @@ require_once './../database/configuration.php';
             </div>
 
             <div class="mb-4">
-                <label for="supplier" class="block text-gray-700 font-medium mb-2">supplier</label>
-                <select name="supplier" id="supplier"  class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required>
+                <label for="supplierpr" class="block text-gray-700 font-medium mb-2">supplier</label>
+                <select name="supplierpr" id="supplierpr"  class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required>
                 <option value="choose a supplier" selected>choose a supplier</option>
                     <?php
                 $sql = "SELECT supplier_name FROM supplier";
@@ -31,6 +31,25 @@ require_once './../database/configuration.php';
                 while ($option = $result->fetch_assoc()) {
                     echo "
                    <option id='$option[supplier_name]'>$option[supplier_name]</option>
+                    ";
+                }
+                ?>
+                </select>
+            </div>
+            <div class="mb-4">
+                <label for="CategoryPr" class="block text-gray-700 font-medium mb-2">Category</label>
+                <select name="CategoryPr" id="CategoryPr"  class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required>
+                <option value="choose a supplier" selected>choose a supplier</option>
+                    <?php
+                $sql = "SELECT category_name FROM category";
+                $result = $conn->query($sql);
+                if (!$result) {
+                    die("Invalid query:" . $conn->error);
+                }
+                //read data of each row
+                while ($option = $result->fetch_assoc()) {
+                    echo "
+                   <option id='$option[category_name]'>$option[category_name]</option>
                     ";
                 }
                 ?>
