31bb5cb316610393456ff73d2069ef65c9e59486
anouar el barry <elbarryanwar37@gmail.com>
2024-12-20 01:08:43 +0100

    add new order

--- src/Controllers/order_Controller.php
+++ src/Controllers/order_Controller.php
@@ -63,6 +63,49 @@ if (isset($_GET['id'])) {
         echo "Database error: " . $e->getMessage();
     }
 }
+if ($_SERVER['REQUEST_METHOD'] === 'POST' &&  isset($_POST['ajoute']) ) {
+  // Retrieve form data
+  $customer = $_POST['customer_email'];
+  $status = $_POST['status'];
+  $quantity = $_POST['quantity'];
+  $supplier = $_POST['supplier'];
+  $product = $_POST['product'];
+  $OrderDate = $_POST['OrderDate'];
+  // Validate and sanitize data
+  $customer = $conn->real_escape_string(trim($customer));
+  $status = $conn->real_escape_string(trim($status));
+  $quantity = $conn->real_escape_string(trim($quantity));
+  $supplier = $conn->real_escape_string(trim($supplier));
+  $product = $conn->real_escape_string(trim($product));
+  $OrderDate = $conn->real_escape_string(trim($OrderDate));
+  
+ // Fetch the IDs for customer, supplier, and product
+ $sql1 = "SELECT id FROM customer WHERE email = '$customer'";
+ $result1 = $conn->query($sql1);
+ $customerId = $result1->fetch_assoc()['id']; // Get the customer ID
+
+ $sql2 = "SELECT id FROM supplier WHERE supplier_name = '$supplier'";
+ $result2 = $conn->query($sql2);
+ $supplierId = $result2->fetch_assoc()['id']; // Get the supplier ID
+
+ $sql3 = "SELECT id FROM product WHERE name = '$product'";
+ $result3 = $conn->query($sql3);
+ $productId = $result3->fetch_assoc()['id']; // Get the product ID
+
+
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+        $insertSql = "INSERT INTO `orders` (customer_id,order_status,product_id,quantity_ordered,supplier_id,order_date) 
+        VALUES ('$customerId', '$status','$productId','$quantity','$supplierId','$OrderDate')";
+if ($conn->query($insertSql) === TRUE) {
+echo "order for Customer nbr:".$customerId." added successfully!";
+} else {
+echo "Error: " . $insertSql . "<br>" . $conn->error;
+}
+    }
       ?>
       </p>
 
