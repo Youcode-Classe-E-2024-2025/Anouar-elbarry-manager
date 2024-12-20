039d6a72da4553164426009e2401b6b03f9f98e7
anouar el barry <elbarryanwar37@gmail.com>
2024-12-20 14:58:58 +0100

    add input for action

--- src/forms/users/add_user.php
+++ src/forms/users/add_user.php
@@ -1,6 +1,7 @@
 <div class="hidden pt-14 adduser_modal h-screen sm:ml-64 overflow-x-auto shadow-md sm:rounded-lg px-8">
         <h2 class="text-2xl font-bold mb-4">User Information</h2>
         <form action="./../src\Controllers\user_Controller.php" class="grid grid-cols-2 gap-6" method="POST">
+        <input type="hidden" name="action" value="add_user">
             <div class="mb-4 col-start-1">
                 <label for="username" class="block text-gray-700 font-medium mb-2">Username</label>
                 <input type="text" id="username" name="username" class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" required>
