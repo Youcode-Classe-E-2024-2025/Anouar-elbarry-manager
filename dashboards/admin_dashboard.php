fa6b1fd79e3026b4b83f162f79d83878f62d3165
anouar el barry <elbarryanwar37@gmail.com>
2024-12-22 23:01:18 +0100

    remove statistics

--- dashboards/admin_dashboard.php
+++ dashboards/admin_dashboard.php
@@ -263,21 +263,6 @@ require_once('./../src\forms\users\add_user.php');
                         <span class="flex-1 ms-3 whitespace-nowrap">Add Customer</span>
                     </a>
                 </li>
-                <!-- Statistics -->
-                <li>
-                    <a href="#"
-                        class="flex Statistics items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                        <svg xmlns="http://www.w3.org/2000/svg"
-                            class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-orange-600 dark:group-hover:text-white"
-                            fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 0 1 3 19.875v-6.75ZM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V8.625ZM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V4.125Z" />
-                        </svg>
-
-                        <span class="flex-1 ms-3 whitespace-nowrap">Statistics</span>
-                    </a>
-                </li>
-
 
 
 
